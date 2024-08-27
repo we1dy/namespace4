e8a535d5237acf12e35a73cf06cb61aa78ec3298
we1dy <ladyarboleda26@gmail.com>
2024-08-28 01:48:59 +0800

    Add project files.

---
+++ namespace4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int num, reverse = 0;
+			Console.WriteLine("Enter a Number: ");
+			num = int.Parse(Console.ReadLine());
+
+			while (num != 0)
+			{
+				reverse = reverse * 10;
+				reverse = reverse + num % 10;
+				num = num / 10;
+			}
+
+			Console.WriteLine("Reverse of Entered Number is: " + reverse);
+			Console.ReadLine();
+		}
+	}
+}
